1be8b5438888ef7134d3e2b5059e4919ab28e552
hratsch <hugh.ratsch.18@gmail.com>
2025-05-26 17:01:35 +0000

    added pygame fps clock

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     # initialize pygame
     pygame.init()
 
+    # initalize game clock
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     # get a new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -23,6 +27,8 @@ def main():
         pygame.Surface.fill(screen, color="black")
         # refreshes screen
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
