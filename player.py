d146b5a5d33fb7b1697a5dd8a156d958d26c3f2a
hratsch <hugh.ratsch.18@gmail.com>
2025-05-26 19:26:41 +0000

    removed extra lines from player class and implemented super

--- player.py
+++ player.py
@@ -4,9 +4,6 @@ from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.x = x
-        self.y = y
-        self.radius = PLAYER_RADIUS
         self.rotation = 0
 
     def triangle(self):
