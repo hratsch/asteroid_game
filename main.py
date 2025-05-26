8ec4fa7395228abe6aad3235b78ecc7af20ccf48
hratsch <hugh.ratsch.18@gmail.com>
2025-05-26 14:29:36 +0000

    created GUI

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import pygame
+from constants import *
 
 def main():
+    # display welcome text
     print("Starting Asteroids!")
-    
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # initialize pygame
+    pygame.init()
+
+    # get a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # create game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # fill screen with solid black color
+        pygame.Surface.fill(screen, color="black")
+        # refreshes screen
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
