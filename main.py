6c75f15496d95f3e726ada2de8eeb4e156ada955
hratsch <hugh.ratsch.18@gmail.com>
2025-05-28 00:37:22 +0000

    added asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     # display welcome text
@@ -15,18 +17,24 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
-    # two groups
+    # groups 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # implement group class
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     # get a new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # implement Player object
     Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Asteroid Field object
+    AsteroidField()
 
     # create game loop
     while True:
@@ -37,22 +45,13 @@ def main():
         # fill screen with solid black color
         pygame.Surface.fill(screen, color="black")
 
-        # re-render player // being phased out w/ groups
-        #player.draw(screen)
-        #player.update(dt)
-
         # using groups
         updatable.update(dt)
         for objects in drawable:
             objects.draw(screen)
-            
-
-
 
         # refreshes screen
         pygame.display.flip()
-
-       
 
         # set fps to 60
         dt = game_clock.tick(60) / 1000
