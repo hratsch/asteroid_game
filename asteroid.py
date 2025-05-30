87a42d690773c686249a383c3e0ff4ad25b6d8d9
hratsch <hugh.ratsch.18@gmail.com>
2025-05-30 04:11:32 +0000

    bug fix attempt #3 for split method

--- asteroid.py
+++ asteroid.py
@@ -13,7 +13,7 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
-    def split(self, dt):
+    def split(self):
         
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
@@ -25,5 +25,5 @@ class Asteroid(CircleShape):
         new_radius = self.radius - ASTEROID_MIN_RADIUS # self.radius is passed as old radius
         a1 = Asteroid(self.position.x, self.position.y, new_radius)
         a2 = Asteroid(self.position.x, self.position.y, new_radius)
-        a1.position += v1 * 1.2 * dt
-        a2.position += v2 * 1.2 * dt
+        a1.velocity += v1 * 1.2
+        a2.velocity += v2 * 1.2
